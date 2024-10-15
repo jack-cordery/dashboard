d4941d0c89430a71cfe30c7de157d7c0677cfe13
Jack Cordery <jack.redolf@gmail.com>
2024-10-15 12:05:12 +0100

    Error handling

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -24,11 +24,58 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-            INSERT INTO invoices (customer_id, amount, status, date) 
+  try {
+    await sql`
+            INSERT INTO invoices (xcustomer_id, amount, status, date) 
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
+  } catch (error) {
+    return {
+      message: `Database Error: Failed to create invoices: ${error instanceof Error ? error.message : 'Unknown error'}`,
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  } catch (error) {
+    return {
+      message: `Database Error: Failed to update invoices: ${error instanceof Error ? error.message : 'Unknown error'}`,
+    };
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    return {
+      message: `Database Error: Failed to delete invoices: ${error instanceof Error ? error.message : 'Unknown error'}`,
+    };
+  }
+  revalidatePath('/dashboard/invoices');
 }
